d248b0e7703b8463e8c30c7bafc62314698f151d
Mira <mira.peryshko@metropolia.fi>
2024-10-30 11:23:13 +0200

    Complete FE Pair Programming 1 Part 2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import logo from './images/logo.svg';
+
+function App() {
+  return (
+    <>
+      <h3>React Practice</h3>
+      <img src={logo} className="App-logo" alt="logo" />
+    </>
+  );
+}
+
+export default App;
