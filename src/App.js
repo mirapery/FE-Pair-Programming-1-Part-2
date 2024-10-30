15b403f30ac1b05b5f0cf7c6a6751a4023c8089b
Mira <mira.peryshko@metropolia.fi>
2024-10-30 12:07:33 +0200

    keskeneräinen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import './App.css';
-import logo from './images/logo.svg';
+import CreditCard from './CreditCard/CreditCard';
 
 function App() {
   return (
     <>
-      <h3>React Practice</h3>
-      <img src={logo} className="App-logo" alt="logo" />
+      <CreditCard
+        type="Visa"
+        number="0123456789018845"
+        expirationMonth={3}
+        expirationYear={2021}
+        bank="BNP"
+        owner="Maxence Bouret"
+        bgColor="#11aa99"
+        color="white"
+      />
+
+      <CreditCard
+        type="Master Card"
+        number="0123456789010995"
+        expirationMonth={3}
+        expirationYear={2021}
+        bank="N26"
+        owner="Maxence Bouret"
+        bgColor="#eeeeee"
+        color="#222222"
+      />
+
+      <CreditCard
+        type="Visa"
+        number="0123456789016984"
+        expirationMonth={12}
+        expirationYear={2019}
+        bank="Name of the Bank"
+        owner="Firstname Lastname"
+        bgColor="#ddbb55"
+        color="white"
+      />
     </>
   );
 }
---
+++ src/CreditCard/CreditCard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import imgVisa from '../images/visa.png';
+import imgMaster from '../images/master.png';
+
+function CreditCard(props) {
+    const number = props.number.toString();
+    console.log(props);
+    const last4Digits = number.slice(-4);
+    const boxColor = {backgroundColor: props.bgColor};
+    const textColor = {color: props.color};
+    const combinedStyles = {...boxColor, ...textColor};
+
+    return (
+        <div>
+            <CreditCard style={combinedStyles}>
+                <img src={props.type === "Visa" ? imgVisa : imgMaster} alt="logo" />
+                <p>.... .... .... {last4Digits}</p>
+                <ul>
+                    <li>{props.expirationMonth}</li>
+                    <li>{props.expirationYear}</li>
+                    <li>{props.bank}</li>
+                    <li>{props.owner}</li>
+                </ul>
+                <p></p>
+            </CreditCard>
+        </div>
+    );
+}
+
+export default CreditCard;
